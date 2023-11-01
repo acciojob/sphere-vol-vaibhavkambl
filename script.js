d6c6524ae04d8e668b43c13b4be09ffb4d385b8e
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-01 12:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+  var radius = document.getElementById("radius").value;
+
+	var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+	 document.getElementById("volume").value = volume.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
